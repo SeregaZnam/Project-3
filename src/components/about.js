576764d0049768e8c7ea073c6798e116de069dc3
SeregaZnam <Sergey-znamensk@yandex.ru>
2017-10-31 00:36:04 +0400

    step 2

--- src/components/about.js
+++ src/components/about.js
@@ -4,8 +4,22 @@ export default class About extends Component {
     render () {
         return (
             <div className='about'>
-                About
+                <div className="about__block">
+                    <div className="about__block_title">Yoga Philosophy</div>
+                    <div className="about__block_slogan">Body is your temple. Keep it pure and clean for the soul to reside in.</div>
+                    <div className="about__block_text">Feel free to start your project using this <span className="about__block_underline">Hatha Yoga Template</span> that goes with two packages! Tell everyone about yoga lifestyle and yoga philosophy.</div>
+                    <div className="about__block_footer">Do your practice and reach harmony!</div>
+                </div>
+                <div className="about__block">
+                    <div className="about__block_title">About Us</div>
+                    <div className="about__block_text"><span className="about__block_bold">Hatha Yoga</span> is one of <span className="about__block_underline">free website templates</span> created by TemplateMonster.com team. This website template is optimized for 1024X768 resolution. It is also XHTML & CSS valid. This website template has </div>
+                    <div className="about__block_icons">
+                        <div className="about__block_up"></div>
+                        <div className="about__block_down"></div>
+                    </div>
+                </div>
+                <div className='about__exit'></div>
             </div>
-        );
+         );
     }
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,23 @@ export default class Footer extends Component {
     render () {
         return (
             <div className='footer'>
+                <div className="content">
+                    <div className="footer__block-left">
+                        <div className="footer__block_center">
+                            Background:
+                            <div className="footer__number">1</div>
+                            <div className="footer__number">2</div>
+                            <div className="footer__number">3</div>
+                        </div>
+                            <div className="footer__block-left_icons">
+                            <div className="footer_icon"><a href="#"></a></div>
+                            <div className="footer_icon"><a href="#"></a></div>
+                            <div className="footer_icon"><a href="#"></a></div>
+                            <div className="footer_icon"><a href="#"></a></div>
+                        </div>
+                    </div>
+                    <div className="footer__block-right"></div>
+                </div>
             </div>
         );
     }
